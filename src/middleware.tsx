3921e82811ec6d03cf00ab2aa2ca2bb465ed1176
yuichiro <zirogingin@gmail.com>
2024-03-09 16:02:53 +0900

    update

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-    matcher: '/labels/:path*',
+    matcher: '/',
 };
 
 export function middleware(req: NextRequest) {
