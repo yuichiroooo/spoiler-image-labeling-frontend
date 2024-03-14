32b24465148ade83be0c3efe6da114eb7bda17fe
yuichiro <zirogingin@gmail.com>
2024-03-14 14:09:29 +0900

    update

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <header className="font-sans flex justify-center mt-10">
-            <h1 className="text-4xl font-bold">Spoiler Label Annotation</h1>
+        <header className="font-sans flex justify-center mt-8">
+            <Link href="/">
+                <h1 className="text-4xl font-bold cursor-pointer">Spoiler Label Annotation</h1>
+            </Link>
         </header>
     )
 }
